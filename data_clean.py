c0817d72ae01c85936ad79b94ba3a5dc3b4c297c
Adedolapo Okanlawon <37551544+riddy18@users.noreply.github.com>
2019-04-28 16:27:29 -0500

    Add files via upload

--- data_clean.py
+++ data_clean.py
@@ -1,2 +1,39 @@
-# JSON -> CSV WITH DATA THAT CORRESPONDS WITH OUR DATABASE. EASIEST IS TO HAVE ONE CSV PER TABLE. 
+# JSON -> CSV WITH DATA THAT CORRESPONDS WITH OUR DATABASE. EASIEST IS TO HAVE ONE CSV PER TABLE.
 
+
+
+import json
+import pandas as pd
+import csv
+
+
+tweets_data_path = 'data.txt'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+
+
+# print ( len(tweets_data) )
+
+
+tweets = pd.DataFrame()
+
+tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+tweets['location'] = map(lambda tweet: tweet['location'], tweets_data)
+tweets['date'] = map(lambda tweet: tweet['created_at'], tweets_data)
+
+
+
+#for key, val in tweets.items():
+    #print (list(map(key, val)))
+
+
+
+
+tweets.to_csv(r'/home/riddy/Downloads/Twitter-Text-Mining-master/tweets.csv', index = None, header = True)
