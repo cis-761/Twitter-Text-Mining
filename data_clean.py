e3cd39a1e367c0504c1616f868507f649b63112d
Luis Bobadilla <happystep@ksu.edu>
2019-04-29 15:27:56 -0500

    this should clean users

--- data_clean.py
+++ data_clean.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import ast
 
 tweets_data_path = 'data.txt'
 
@@ -19,9 +20,34 @@ tweets['location'] = list(map(lambda tweet: tweet['coordinates'], tweets_data))
 tweets['favorite'] = list(map(lambda tweet: tweet['favorited'], tweets_data))
 tweets['date'] = list(map(lambda tweet: tweet['created_at'], tweets_data))
 
-user = pd.DataFrame()
+users = pd.DataFrame()
+
+users['user'] = list(map(lambda tweet: tweet['user'], tweets_data))
+
+users['user'] = users['user'].astype(str)
+
+user_data = [] 
+
+for i in users.user:
+    data = ast.literal_eval(i)
+    s1 = json.dumps(data)
+    user = json.loads(s1)
+    user_data.append(user)
+
+# user information we need to collect 
+# name, screen_name, geo_enabled, verified
+
+users['name'] = list(map(lambda user: user['name'], user_data))
+users['screen_name'] = list(map(lambda user: user['screen_name'], user_data))
+users['geo_enabled'] = list(map(lambda user: user['geo_enabled'], user_data))
+users['verified'] = list(map(lambda user: user['verified'], user_data))
+
+user_frame = users.drop('user', axis=1)
+
+print(users_frame.columns)
+print(users_frame.shape)
 
 
-
-
-tweets.to_csv('test.csv', index = None, header = True)
+# This is where we write out the DataFrames to CSV. 
+users_frame.to_csv('user.csv', index=None, header=True)
+tweets.to_csv('tweets.csv', index = None, header = True)
