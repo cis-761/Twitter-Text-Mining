7c0a7085ee6d7c92e8f9b071427f1e0da19ddc7a
Luis Bobadilla <happystep@ksu.edu>
2019-05-01 09:50:42 -0500

    locations cleaned

--- data_clean.py
+++ data_clean.py
@@ -20,6 +20,30 @@ tweets['location'] = list(map(lambda tweet: tweet['coordinates'], tweets_data))
 tweets['favorite'] = list(map(lambda tweet: tweet['favorited'], tweets_data))
 tweets['date'] = list(map(lambda tweet: tweet['created_at'], tweets_data))
 
+# getting RT Column
+retweet_list = []
+for k, v in tweets.text.iteritems():
+    if v[:2] == "RT":
+        retweet_list.append(True)
+    else:
+        retweet_list.append(False)
+
+tweets['rt'] = retweet_list # this should make this extra column ? 
+
+# Cleaning location 
+location_list = []
+for k, v in tweets.location.iteritems():
+    if v is None:
+        location_list.append("")
+    else:
+        location_list.append(v['coordinates'])
+
+tweets.drop(labels='location', axis='columns', inplace=True)
+tweets['location'] = location_list
+
+
+# start processing users
+
 users = pd.DataFrame()
 
 users['user'] = list(map(lambda tweet: tweet['user'], tweets_data))
@@ -39,14 +63,10 @@ for i in users.user:
 
 users['name'] = list(map(lambda user: user['name'], user_data))
 users['screen_name'] = list(map(lambda user: user['screen_name'], user_data))
-users['geo_enabled'] = list(map(lambda user: user['geo_enabled'], user_data))
+users['geo_enabled'] = list(map(lambda user: user['geo_enabled'], user_data)) #clean out as string-coordinates not null, those null stay null
 users['verified'] = list(map(lambda user: user['verified'], user_data))
 
 users_frame = users.drop('user', axis=1)
-
-print(users_frame.columns)
-print(users_frame.shape)
-
 
 # This is where we write out the DataFrames to CSV. 
 users_frame.to_csv('user.csv', index=None, header=True)
