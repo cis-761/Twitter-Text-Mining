d1e387d51bd260c76079dfa909ef1b47b32ce890
Yihong Theis <theis@civicplus.com>
2019-05-11 21:11:45 -0500

    finish trendcluster.py

--- trendscluster.py
+++ trendscluster.py
@@ -2,16 +2,253 @@ from scipy.cluster.vq import kmeans2, whiten
 import numpy as np  # Data manipulation
 import decimal
 import pandas as pd           # Dataframe manipulatio
-import geopandas
-from shapely.geometry import Point
+import math
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+from sklearn.cluster import KMeans
+import mpld3
 import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
+import gensim, spacy, logging, warnings
+import gensim.corpora as corpora
+from gensim.utils import lemmatize, simple_preprocess
+from gensim.models import CoherenceModel
+from pprint import pprint
+from sklearn.manifold import TSNE
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import Label
+from bokeh.io import output_notebook
+from matplotlib import colors as mcolors
 
 
+tweet_read =pd.read_csv('tweets.csv', delimiter=',' , )
+#get not null location tweets
+tweet_withlocation = tweet_read[tweet_read.location.notnull()]
+#important_text= ['flu', 'flushot', 'sick', 'ill', 'cold', 'influenza']
+tweet_final =tweet_withlocation[tweet_withlocation['text'].str.contains('flu|flushot|sick|ill|cold|influenza', regex = True)]
+documents = tweet_final['text'].tolist()
 
-# #print(whiten(coordinates))
-# coordinates = np.column_stack((lon, lat))
-# #print(coordinates)
-# x, y = kmeans2(whiten(coordinates), 3, iter = 20)
-# print(x, y)
-# plt.scatter(coordinates[:,0], coordinates[:,1], c=y);
-# plt.show()
+# texts =tweet_withlocation['text'].tolist()
+texts = tweet_read['text'].tolist()
+real_texts =[]
+wordslist =['flu', 'flushot’, ‘sick’, ‘ill’, ‘avian flu’, ‘bunged up’, ‘catarrh’, ‘chesty’, ‘chill’, ‘cold’, \
+‘congested’, ‘cough’, ‘coughing fit’,‘frog’, ‘hacking cough’, ‘head cold’, \
+‘influenza’, ‘runny’, ‘sneeze’, ‘sniff’, ‘sniffle’, ‘swine flu’, ‘common cold', 'coughing', 'fever', 'hayfever', \
+            'runny rose', 'sore throat', 'headaches', 'muscle aches', 'stuffy nose']
+for i in texts:
+    for m in wordslist:
+        if m in i:
+            real_texts.append(m)
+print(len(real_texts))
+texts2 = tweet_read['text'].tolist()
+
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True)
+        yield(sent)
+data_words = list(sent_to_words(real_texts))
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+def process_words(texts, stop_words='english', allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]
+    return texts_out
+
+data_ready = process_words(data_words)
+id2word = corpora.Dictionary(data_ready)
+
+# Create Corpus: Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in data_ready]
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=4,
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=10,
+                                           passes=10,
+                                           alpha='symmetric',
+                                           iterations=100,
+                                           per_word_topics=True)
+
+pprint(lda_model.print_topics())
+def format_topics_sentences(ldamodel=None, corpus=corpus, texts=texts2):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data_ready)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+print(df_dominant_topic.head(10))
+pd.options.display.max_colwidth = 100
+
+sent_topics_sorteddf_mallet = pd.DataFrame()
+sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+for i, grp in sent_topics_outdf_grpd:
+    sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet,
+                                             grp.sort_values(['Perc_Contribution'], ascending=False).head(1)],
+                                            axis=0)
+
+# Reset Index
+sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+# Format
+sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Representative Text"]
+
+# Show
+print(sent_topics_sorteddf_mallet.head(10))
+doc_lens = [len(d) for d in df_dominant_topic.Text]
+
+topic_weights = []
+for i, row_list in enumerate(lda_model[corpus]):
+    topic_weights.append([w for i, w in row_list[0]])
+
+# Array of topic weights
+arr = pd.DataFrame(topic_weights).fillna(0).values
+
+# Keep the well separated points (optional)
+arr = arr[np.amax(arr, axis=1) > 0.35]
+
+# Dominant topic number in each doc
+topic_num = np.argmax(arr, axis=1)
+
+# tSNE Dimension Reduction
+tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=.99, init='pca')
+tsne_lda = tsne_model.fit_transform(arr)
+
+# Plot the Topic Clusters using Bokeh
+output_notebook()
+n_topics = 4
+mycolors = np.array([color for name, color in mcolors.TABLEAU_COLORS.items()])
+plot = figure(title="t-SNE Clustering of {} LDA Topics".format(n_topics),
+              plot_width=900, plot_height=700)
+plot.scatter(x=tsne_lda[:,0], y=tsne_lda[:,1], color=mycolors[topic_num])
+#output_file("foo.html")
+show(plot)
+
+from collections import Counter
+topics = lda_model.show_topics(formatted=False)
+data_flat = [w for w_list in data_ready for w in w_list]
+counter = Counter(data_flat)
+
+out = []
+for i, topic in topics:
+    for word, weight in topic:
+        out.append([word, i , weight, counter[word]])
+
+df = pd.DataFrame(out, columns=['word', 'topic_id', 'importance', 'word_count'])
+
+# Plot Word Count and Weights of Topic Keywords
+fig, axes = plt.subplots(2, 2, figsize=(16,10), sharey=True, dpi=160)
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+for i, ax in enumerate(axes.flatten()):
+    ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
+    ax_twin = ax.twinx()
+    ax_twin.bar(x='word', height="importance", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.2, label='Weights')
+    ax.set_ylabel('Word Count', color=cols[i])
+    ax_twin.set_ylim(0, 0.030); ax.set_ylim(0, 3500)
+    ax.set_title('Topic: ' + str(i), color=cols[i], fontsize=16)
+    ax.tick_params(axis='y', left=False)
+    ax.set_xticklabels(df.loc[df.topic_id==i, 'word'], rotation=30, horizontalalignment= 'right')
+    ax.legend(loc='upper left'); ax_twin.legend(loc='upper right')
+
+fig.tight_layout(w_pad=2)
+fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=22, y=1.05)
+#plt.show()
+
+def topics_per_document(model, corpus, start=0, end=1):
+    corpus_sel = corpus[start:end]
+    dominant_topics = []
+    topic_percentages = []
+    for i, corp in enumerate(corpus_sel):
+        topic_percs, wordid_topics, wordid_phivalues = model[corp]
+        dominant_topic = sorted(topic_percs, key = lambda x: x[1], reverse=True)[0][0]
+        dominant_topics.append((i, dominant_topic))
+        topic_percentages.append(topic_percs)
+    return(dominant_topics, topic_percentages)
+
+dominant_topics, topic_percentages = topics_per_document(model=lda_model, corpus=corpus, end=-1)
+
+# Distribution of Dominant Topics in Each Document
+df = pd.DataFrame(dominant_topics, columns=['Document_Id', 'Dominant_Topic'])
+dominant_topic_in_each_doc = df.groupby('Dominant_Topic').size()
+df_dominant_topic_in_each_doc = dominant_topic_in_each_doc.to_frame(name='count').reset_index()
+
+# Total Topic Distribution by actual weight
+topic_weightage_by_doc = pd.DataFrame([dict(t) for t in topic_percentages])
+df_topic_weightage_by_doc = topic_weightage_by_doc.sum().to_frame(name='count').reset_index()
+
+# Top 3 Keywords for each Topic
+topic_top3words = [(i, topic) for i, topics in lda_model.show_topics(formatted=False)
+                                 for j, (topic, wt) in enumerate(topics) if j < 3]
+
+df_top3words_stacked = pd.DataFrame(topic_top3words, columns=['topic_id', 'words'])
+df_top3words = df_top3words_stacked.groupby('topic_id').agg(', \n'.join)
+df_top3words.reset_index(level=0,inplace=True)
+
+from matplotlib.ticker import FuncFormatter
+
+# Plot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4), dpi=120, sharey=True)
+
+# Topic Distribution by Dominant Topics
+ax1.bar(x='Dominant_Topic', height='count', data=df_dominant_topic_in_each_doc, width=.5, color='firebrick')
+ax1.set_xticks(range(df_dominant_topic_in_each_doc.Dominant_Topic.unique().__len__()))
+tick_formatter = FuncFormatter(lambda x, pos: 'Topic ' + str(x)+ '\n' + df_top3words.loc[df_top3words.topic_id==x, 'words'].values[0])
+ax1.xaxis.set_major_formatter(tick_formatter)
+ax1.set_title('Number of Documents by Dominant Topic', fontdict=dict(size=10))
+ax1.set_ylabel('Number of Documents')
+ax1.set_ylim(0, 1000)
+
+# Topic Distribution by Topic Weights
+ax2.bar(x='index', height='count', data=df_topic_weightage_by_doc, width=.5, color='steelblue')
+ax2.set_xticks(range(df_topic_weightage_by_doc.index.unique().__len__()))
+ax2.xaxis.set_major_formatter(tick_formatter)
+ax2.set_title('Number of Documents by Topic Weightage', fontdict=dict(size=10))
+
+#plt.show()
+
+import pyLDAvis.gensim
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary=lda_model.id2word)
+pyLDAvis.save_html(vis, 'lda.html')
